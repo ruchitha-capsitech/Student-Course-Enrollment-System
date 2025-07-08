d2d429d21e6237b0fb94626128644d9b3f09bb93
Ruchitha Kalluri Sunil <ruchitha.kalluri@capsitech.com>
2025-07-08 17:33:29 +0530

    Update MongoDBSettings.cs

--- backend/Models/MongoDBSettings.cs
+++ backend/Models/MongoDBSettings.cs
@@ -1,9 +1,31 @@
-﻿namespace Student_course_enrollment.Models
+// ﻿namespace Student_course_enrollment.Models
+// {
+//     public class MongoDbSettings
+//     {
+//         public string ConnectionString { get; set; } = null!;
+//         public string DatabaseName { get; set; } = null!;
+//     }
+
+// }
+namespace Student_course_enrollment.Models
 {
-    public class MongoDbSettings
+    public class MongoDbSettings : IMongoDbSettings
     {
         public string ConnectionString { get; set; } = null!;
         public string DatabaseName { get; set; } = null!;
+        public string StudentCollectionName { get; set; } = null!;
+        public string CourseCollectionName { get; set; } = null!;
+        public string EnrollmentCollectionName { get; set; } = null!;
+        public string UserCollectionName { get; set; } = null!;
     }
 
+    public interface IMongoDbSettings
+    {
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+        string StudentCollectionName { get; set; }
+        string CourseCollectionName { get; set; }
+        string EnrollmentCollectionName { get; set; }
+        string UserCollectionName { get; set; }
+    }
 }
